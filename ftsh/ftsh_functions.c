f91cdb69af514e8b35e6c59eaf12b3d667d14feb
Max Bronckers <mbronckers@cs.uchicago.edu>
2018-05-01 23:52:12 -0500

    Added functional shell

---
+++ ftsh/ftsh_functions.c
@@ -0,0 +1,89 @@
+/***************************************************************************//**
+
+	@file         ftsh_functions.c
+
+  @author       Max Bronckers
+
+	@date         Thursday,  1 May 2018
+
+  @brief        FTSH functions
+
+*******************************************************************************/
+
+#include "ftsh.h"
+#include "ftsh_functions.h"
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &ftsh_help,
+  &ftsh_exit
+};
+
+/**
+	@brief Builtin command: print help.
+	@param args List of args.  Not examined.
+	@return Always returns 1, to continue executing.
+ */
+int ftsh_help(char **args)
+{
+  int i;
+  printf("full-text shell\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < ftsh_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+/**
+	@brief Builtin command: exit.
+	@param args List of args.  Not examined.
+	@return Always returns 0, to terminate execution.
+ */
+int ftsh_exit(char **args)
+{
+  return 0;
+}
+
+
+/**
+	@brief Number of built-in functions.
+	@return Returns number of functions.
+ */
+int ftsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+/**
+   @brief Execute shell built-in or launch program.
+   @param args Null terminated list of arguments.
+   @return 1 if the shell should continue running, 0 if it should terminate
+ */
+int ftsh_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < ftsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return ftsh_launch(args);
+}
---
+++ ftsh/ftsh_functions.h
@@ -0,0 +1,32 @@
+#ifndef FTSH_FUNCTIONS
+#define FTSH_FUNCTIONS
+
+/***************************************************************************//**
+
+	@file         ftsh_functions.h
+
+  @author       Max Bronckers
+
+	@date         Thursday,  1 May 2018
+
+  @brief        FTSH supported commands header file
+
+*******************************************************************************/
+
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char *builtin_str[];
+
+int ftsh_help(char **args);
+
+int ftsh_exit(char **args);
+
+int ftsh_num_builtins();
+
+int ftsh_execute(char **args);
+
+#endif
