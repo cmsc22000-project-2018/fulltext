5091ed492e4a8b4e94eb82d47cc3da08709a75ec
Christina T Ford <fordc@linux1.cs.uchicago.edu>
2018-05-03 22:55:50 -0500

    filled in functions and updated header comments

--- linked_list/include/match.h
+++ linked_list/include/match.h
@@ -5,44 +5,134 @@
 typedef struct {
     char* word;
     int line;
-    match* prev;
-    match* next;
 } match;
 
-/* Create new match */
+/* 
+ * Creates new match 
+ *
+ * Params: 
+ *  - char* word: the match string
+ *  - int line: the line the match is on
+ *
+ * Returns:
+ *  - a pointer to the match
+ */
 match* new_match(char* word, int line);
 
-/* Initialize match */
+/* 
+ * Initializes match 
+ *
+ * Params:
+ *  - match* match: pointer to match
+ *  - char* word: the match string
+ *  - int line: the line the match is on
+ */
 void init_match(match* match, char* word, int line);
 
-/* Free match */
-int free_match(match* match);
+/* 
+ * Frees match 
+ *
+ * Params:
+ *  - match* match: pointer to match
+ */
+void free_match(match* match);
 
-/* Find word in match struct */
+/* 
+ * Finds word in match struct 
+ *
+ * Params:
+ *  - match* match: pointer to match
+ *
+ * Returns:
+ *  - string in match struct
+ */
 char* get_word(match* match);
 
-/* Return the line number of match struct */
+/* 
+ * Returns the line number of match struct 
+ *
+ * Params:
+ *  - match* match: pointer to match
+ *
+ * Returns:
+ *  - int representing line number
+ */
 int get_line(match* match);
 
-/* Go to next item in linked list */
-match* next_match(match* match);
+/* 
+ * Goes to next item in linked list 
+ *
+ * Params:
+ *  - match* match: pointer to match
+ *  - GList* matches: doubly linked list (dll) of matches
+ *
+ * Returns:
+ *  - pointer to next match
+ */
+match* next_match(match* match, GList* matches);
 
-/* Go to previous item in linked list */
-match* prev_match(match* match);
+/* 
+ * Goes to previous item in linked list 
+ *
+ * Params:
+ *  - match* match: pointer to match
+ *  - GList* matches: dll of matches
+ *
+ * Returns:
+ *  - pointer to previous match
+ */
+match* prev_match(match* match, GList* matches);
 
-/* Insert at specific index in match struct */
-void insert_at(match* newMatch, int index, GList matches);
+/* 
+ * Inserts at specific index in match struct 
+ * 
+ * Params:
+ *  - match* newMatch: pointer to new match struct
+ *  - int index: index to insert the match into dll
+ *  - GList* matches: dll of matches
+ */
+void insert_at(match* newMatch, int index, GList* matches);
 
-/* Insert at end of match struct */
-void append(match* newMatch, GList matches);
+/* 
+ * Inserts at end of match struct 
+ *
+ * Params:
+ *  - match* newMatch: pointer to new match struct
+ *  - GList* matches: dll of matches
+ */
+void append(match* newMatch, GList* matches);
 
-/* Remove at specific index in match struct */
-void remove_at(int index, GList matches);
+/* 
+ * Removes at specific index in match struct 
+ *
+ * Params:
+ *  - int index: index of match to remove
+ *  - GList* matches: dll of matches
+ */
+void remove_at(int index, GList* matches);
 
-/* Return the match at the given index */
-match* get_at_index(int index, GList matches);
+/* 
+ * Returns the match at the given index 
+ * 
+ * Params:
+ *  - int index: index of desired match
+ *  - GList* matches: dll of matches
+ *
+ * Returns:
+ *  - pointer of match at the index
+ */
+match* get_at_index(int index, GList* matches);
 
-/* Get the index of current match */
-int get_index(match* match);
+/* 
+ * Gets the index of current match 
+ * 
+ * Params:
+ *  - match* match: match struct at unknown index
+ *  - GList* matches: dll of matches
+ *
+ * Returns:
+ *  - int index of match
+ */
+int get_index(match* match, GList* matches);
 
 #endif /* INCLUDE_MATCH_H_ */
--- linked_list/src/match.c
+++ linked_list/src/match.c
@@ -1,20 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <gmodule.h>
 #include "match.h"
 
 /* See match.h for descriptions of functions */
 
 match* new_match(char* word, int line)
 {
+    match* m = malloc(sizeof(match));
+    
+    if (m == NULL) return NULL;
+    
+    init_match(m, word, line);
 
+    return m;
 }
 
 void init_match(match* match, char* word, int line)
 {
+    if (match == NULL) return;
 
+    match->word = word;
+    match->line = line;
 }
 
 void free_match(match* match)
 {
-
+    free(match);
 }
 
 char* get_word(match* match)
@@ -27,29 +41,33 @@ int get_line(match* match)
     return match->line;
 }
 
-match* next_match(match* match)
+//changed inputs
+match* next_match(match* match, GList* matches)
 {
-
+    GList* cur = g_list_find(matches, match->word);
+    return (cur->next)->data;
 }
 
-match* prev_match(match* match)
+//changed inputs
+match* prev_match(match* match, GList* matches)
 {
-
+    GList* cur = g_list_find(matches, match->word);
+    return (cur->prev)->data;
 }
 //changed inputs
-void insert_at(match* newMatch, int index, GList matches)
+void insert_at(match* newMatch, int index, GList* matches)
 {
     matches = g_list_insert(matches, newMatch, index);
 }
 
 //changed inputs
-void append(match* newMatch, GList matches)
+void append(match* newMatch, GList* matches)
 {
     matches = g_list_append(matches, newMatch);
 }
 
 //changed inputs
-void remove_at(int index, GList matches)
+void remove_at(int index, GList* matches)
 {
     //get element at position
     GList *toDelete = g_list_nth(matches, index);
@@ -59,14 +77,15 @@ void remove_at(int index, GList matches)
 }
 
 //changed inputs
-match* get_at_index(int index, GList matches)
+match* get_at_index(int index, GList* matches)
 {
     GList *toReturn = g_list_nth(matches, index);
     return toReturn -> data;
 }
 
-int get_index(match* match)
+//changed inputs
+int get_index(match* match, GList* matches)
 {
-    int position = g_list_index(l, match);
+    int position = g_list_index(matches, match);
     return position;
 }
