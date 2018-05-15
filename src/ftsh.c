e8f0bbeebc0eb00388fe6cda75b9f668fb3cba2d
Max Bronckers <mbronckers@icloud.com>
2018-05-15 11:27:44 -0500

    Fixed indentation to spaces

--- src/ftsh.c
+++ src/ftsh.c
@@ -1,14 +1,14 @@
 /***************************************************************************//**
 
-	@file         ftsh.c
+    @file         ftsh.c
 
-	@author       Max Bronckers
+    @author       Max Bronckers
 
-	@date         Thursday, 1 May 2018
+    @date         Thursday, 1 May 2018
 
-	@brief        FTSH (Full-Text SHell)
+    @brief        FTSH (Full-Text SHell)
 
-	@compile      make ftsh
+    @compile      make ftsh
 
 *******************************************************************************/
 
@@ -16,104 +16,104 @@
 #include "ftsh_functions.h"
 
 /**
-	@brief Parse input into args.
-	@param input The input.
-	@return Null-terminated array of args.
+    @brief Parse input into args.
+    @param input The input.
+    @return Null-terminated array of args.
  */
 char** ftsh_get_input(char *input) {
-	char **args = malloc(8 * sizeof(char *));
-	char *separator = " ";
-	char *parsed;
-	int index = 0;
+    char **args = malloc(8 * sizeof(char *));
+    char *separator = " ";
+    char *parsed;
+    int index = 0;
 
-	/* Error handling */
-	if (args == NULL) {
-		perror("ftsh malloc failed");
-		exit(1);
-	}
-
-	/* Exit on Ctrl-D */
-	if (input == NULL) {  
-      	printf("\n");
-      	exit(0);
+    /* Error handling */
+    if (args == NULL) {
+        perror("ftsh malloc failed");
+        exit(1);
     }
 
-	parsed = strtok(input, separator);
+    /* Exit on Ctrl-D */
+    if (input == NULL) {  
+        printf("\n");
+        exit(0);
+    }
 
-	while (parsed != NULL) {
-		args[index++] = parsed;
-		parsed = strtok(NULL, separator); 
-	}
+    parsed = strtok(input, separator);
 
-	args[index] = NULL;
-	return args;
+    while (parsed != NULL) {
+        args[index++] = parsed;
+        parsed = strtok(NULL, separator); 
+    }
+
+    args[index] = NULL;
+    return args;
 }
 
 /**
-	@brief Launch a program and wait for it to terminate.
-	@param args Null terminated list of arguments (including program).
-	@return Always returns 1, to continue execution.
+    @brief Launch a program and wait for it to terminate.
+    @param args Null terminated list of arguments (including program).
+    @return Always returns 1, to continue execution.
  */
 int ftsh_launch(char **args)
 {
-	pid_t pid;
-	int status;
+    pid_t pid;
+    int status;
 
-	pid = fork();
-	if (pid == 0) {
-		// Child process
-		if (execvp(args[0], args) == -1) {
-			printf("Your command was not recognized by our system.\n");
-			printf("Please type 'help' to get an overview of the commands.\n");
-			perror("ftsh command execution error");
-		}
-		exit(EXIT_FAILURE);
-	} else if (pid < 0) {
-		// Error forking
-		perror("ftsh: forking error");
-	} else {
-		// Parent process
-		do {
-			waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
+    pid = fork();
+    if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+            printf("Your command was not recognized by our system.\n");
+            printf("Please type 'help' to get an overview of the commands.\n");
+            perror("ftsh command execution error");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        // Error forking
+        perror("ftsh: forking error");
+    } else {
+        // Parent process
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
 
-	return 1;
+    return 1;
 }
 
 /**
-	 @brief Loop getting input and executing it.
+     @brief Loop getting input and executing it.
  */
 void ftsh_loop(void)
 {
-	char *input;
-	char **args;
-	int status;
+    char *input;
+    char **args;
+    int status;
 
-	do {
-		input = readline("ftsh> ");
-		args = ftsh_get_input(input);
-		status = ftsh_execute(args);
+    do {
+        input = readline("ftsh> ");
+        args = ftsh_get_input(input);
+        status = ftsh_execute(args);
 
-		free(input);
-		free(args);
-	} while (status);
+        free(input);
+        free(args);
+    } while (status);
 }
 
 /**
-	 @brief Main entry point.
-	 @param argc Argument count.
-	 @param argv Argument vector.
-	 @return status code
+     @brief Main entry point.
+     @param argc Argument count.
+     @param argv Argument vector.
+     @return status code
  */
 int main(int argc, char **argv)
 {
-	// Load config files, if any.
+    // Load config files, if any.
 
-	// Run command loop.
-	ftsh_loop();
+    // Run command loop.
+    ftsh_loop();
 
-	// Perform any shutdown/cleanup.
+    // Perform any shutdown/cleanup.
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
