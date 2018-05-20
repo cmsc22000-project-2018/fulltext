c8e4c19d1dcbfa76806e6ce3da3deffb268110dd
Ruolin Zheng <ruolinzheng@cs.uchicago.edu>
2018-05-20 16:22:58 -0500

    written basic file flow mimic in filetest.c

--- src/filetest.c
+++ src/filetest.c
@@ -3,35 +3,75 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) 
+// This is a simple mimic of the file flow
+// following ftsh_load(), read_until_next_match(), print_match()
+
+void print_match(char *line, char *word, int line_num, int pos, long int filepos)
 {
-	FILE *fp = fopen("filetest.txt", "r");
-	int line_num = 0;
-	int pos = 0;
-
-	char buf[6];
-	char *line = NULL;
-	char *token, *save_ptr;
-
+	printf("word: %s\nline %d pos %d fp %lu:\n%s\n", 
+		word, line_num, pos, filepos, line);
+}
+// returns 0 when EOF
+int read_until_next_match(FILE *fp, char *word, int line_num)
+{
+    char *line = NULL;
 	size_t len = 0;
-	ssize_t read;
+	ssize_t read = 0;
+	int pos = 0;
+	long int filepos = 0;
+	char *token, *saveptr, *saveln;
 
-	while ((read = getline(&line, &len, fp)) != -1) {
-		line_num++;
-		printf("\non line %d:\n", line_num);
-
-		token = strtok_r(line, " ,.!?\t\n", &save_ptr);
+    while ((read = getline(&line, &len, fp)) != -1) {
+    	saveln = strdup(line);
+    	token = strtok_r(line, " ,.!?\t\n", &saveptr);
 		while (token != NULL) {
-			if ((fgets(buf, 6, stdin) != NULL)
-				&& (strncmp(buf, "next", 4) == 0)) {
-				pos++;
-				printf("\tpos %d  %s\n", pos, token);
-				token = strtok_r(NULL, " ,.!?\t\n", &save_ptr);			
+			pos++;
+			if (strncmp(word, token, strlen(word)) == 0) {
+				filepos = ftell(fp);
+				print_match(saveln, word, line_num, pos, filepos);
 			}
+			token = strtok_r(NULL, " ,.!?\t\n", &saveptr);			
 		}
-
-		pos = 0;
+		free(saveln);
 	}
 	return 0;
 }
+
+int main(int argc, char *argv[]) 
+{
+    char buf[100];
+    char *pinput;
+
+	int save_line_num = 0;
+	int ret = -1;
+
+	if (argc != 2) {
+		printf("usage: ./fulltext <search_term>\n");
+		exit(0);
+	}
+	char *word = argv[1];
+
+    FILE *fp = fopen("filetest.txt", "r");
+
+    while (1) {
+        printf("fulltext> ");
+        pinput = fgets(buf, 100, stdin);
+        if (!pinput) {
+            exit(0);
+        }
+        if (strncmp(buf, "next", 4) == 0) {
+        	save_line_num++;
+        	ret = read_until_next_match(fp, word, save_line_num);
+        	if (ret == 0) {
+        		printf("Reach EOF.\n");
+        		exit(0);
+        	}
+        }
+    }
+
+	return 0;	
+}
+
+
--- src/search.c
+++ src/search.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -9,7 +10,7 @@
 
 // see search.h
 list_t *minimal_read_until_next_match(list_t *match_list, char *search_word,
-	FILE *targetfile_fp)
+	FILE *fp)
 {	
 	char *line = NULL;
 	char *token, *saveptr;
@@ -34,24 +35,27 @@ list_t *minimal_read_until_next_match(list_t *match_list, char *search_word,
 
 			// inefficient search algorithm
 			if (strncmp(search_word, token, strlen(search_word)) == 0) {
-				match *match_node = 
+				match *match_node = new_match(search_word, line_num, pos);
+				append_(match_node, match_list);
+				printf("line %d pos %d  %s\n", line_num, pos, token);
+				return match_list;
 			}
 
 		}
 	}
+	printf("not found\n");
+	return NULL;
 
 }
 
 int main()
 {
-	list_t match_list;
-    list_init(&match_list);
-    match* a = new_match("a", 1, 1);
-    match* b = new_match("b", 1, 1);
-
-    list_append(&match_list, a);
-    list_append(&match_list, b);
-
-    match* nextMatch = next_match(a, &match_list);
-    printf("%s\n", nextMatch->word);
+	char buf[6];
+	list_t *match_list = NULL;
+    //list_init(match_list);
+    FILE *fp = fopen("filetest.txt", "r");
+    while ((fgets(buf, 6, stdin) != NULL)
+				&& (strncmp(buf, "next", 4) == 0)) {
+    	minimal_read_until_next_match(match_list, "fulltext", fp);
+    }
 }
