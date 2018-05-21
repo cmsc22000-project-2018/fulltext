21c45158f02fde66338f317f489bdbc458814874
Max Bronckers <mbronckers@icloud.com>
2018-05-20 22:34:58 -0500

    Updated header file

--- include/search.h
+++ include/search.h
@@ -4,6 +4,13 @@
 #include <string.h>
 #include <stddef.h>
 
+/**
+  Header comments to be written by author of functions @Ruolin
+  */
 void print_match(char *line, char *word, int line_num, int pos, long int filepos);
 
+
+/**
+  Header comments to be written by author of functions @Ruolin
+  */
 int read_until_next_match(FILE *pf, char *word, int line_num);
