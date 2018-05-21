d67c2ef3a9e18dc4c1ef2abca26555c980cf03f0
Ruolin Zheng <ruolinzheng@cs.uchicago.edu>
2018-05-21 15:39:45 -0500

    finished basic implementation of read_until_next_match; does not support multiple occurrences on one line at this point

--- src/search.c
+++ src/search.c
@@ -55,9 +55,48 @@ int single_word_read_until_next_match(FILE *fp, list_t **matches_set,
 	assert(word_set != NULL);
 
 	list_t matches = matches_set[0];
-	char *word = word_set[0];
-	assert(word != NULL);
+	char *search_word = word_set[0];
+	assert(search_word != NULL);
 
 	int ret = minimal_read_until_next_match(fp, matches, search_word, line_num);
 	return ret;
+}
+
+// see search.h
+int read_until_next_match(FILE *fp, list_t **matches_set,
+ char **word_set, int SET_SIZE, int line_num)
+ {
+ 	char *line = NULL;
+	size_t len = 0;
+	ssize_t read = 0;
+	char *token, *saveptr, *saveln;
+	int pos = 0;
+
+	for (int i = 0; i < SET_SIZE; i++) {
+		char *search_word = word_set[i];
+		list_t matches = matches_set[i];
+
+		if (matches == NULL) {
+			list_init(matches);
+		}
+		// start reading line by line
+		while ((read = getline(&line, &len, fp)) != -1) {
+			pos = 0;
+			saveln = strdup(line);
+			token = strtok_r(line, " ,.!?\t\n", &saveptr);
+			while (token != NULL) {
+				pos++;
+
+				// inefficient search algorithm
+				if (strncmp(search_word, token, strlen(search_word)) == 0) {
+					match *match_node = new_match(search_word, line_num, pos);
+					append_(match_node, matches);
+					return 1;
+				}
+				token = strtok_r(NULL, " ,.!?\t\n", &saveptr);
+			}
+			free(saveln);
+		}
+		return 0;
+	}
 }
