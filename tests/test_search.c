80516c786282e2f86d289b6b8b53a3da801d9726
Ruolin Zheng <ruolinzheng@cs.uchicago.edu>
2018-05-25 15:04:30 -0500

    start writing unit tests for find_match and parse file buffered

--- tests/test_search.c
+++ tests/test_search.c
@@ -1,6 +1,26 @@
-#include <criterion/criterion.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
-#include <gmodule.h>
+#include <string.h>
+#include <stddef.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <unistd.h>
+#include "match.h"
+#include "simclist.h"
+
+/* Testing find_match() function */
+Test(search, find_existent_match)
+{
+
+}
+
+Test(search, find_nonexistent_match)
+{
+
+}
+
+/* Testing parse_file_buffered() function */
+Test(search, parse_file_buffered)
+{
+
+}
